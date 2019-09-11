191ced34ad7079c45517e55a7d212b915b7070bc
Krzysztof Krysiak <krysiak86@gmail.com>
2019-09-11 23:50:08 +0200

    Add options

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-//mod print;
-//mod vars;
-//mod types;
-//mod strings;
-//mod tuples;
-mod arrays;
+// mod print;
+// mod vars;
+// mod types;
+// mod strings;
+// mod tuples;
+// mod arrays;
+mod options;
 
 fn main() {
-//    print::run();
-//    vars::run();
-//    types::run();
-//    strings::run();
-//    tuples::run();
-    arrays::run();
+    // print::run();
+    // vars::run();
+    // types::run();
+    // strings::run();
+    // tuples::run();
+    // arrays::run();
+    options::run();
 }
