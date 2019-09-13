ffd5b852cb3a4b51d352b71e08c764724b5fb271
Krzysztof Krysiak <krysiak86@gmail.com>
2019-09-13 14:19:04 +0200

    Add more playground (Result)

--- src/playground.rs
+++ src/playground.rs
@@ -9,12 +9,18 @@ pub fn run() {
     let en1 = EnumEx::C(5);
     let en2 = EnumEx::D { x: 1, y: 2 };
     let x = 10;
-    let z1 = None::<i32>;
-    let z2: Option<i32> = None;
-
     sampleMatch(en1);
     sampleMatch(en2);
+
+    let z1 = None::<i32>;
+    let z2: Option<i32> = None;
     sampleOption(Some(x));
+
+    let input: i32 = 13;
+    match h(input){
+        Ok(result) => println!("Result: {}", result),
+        Err(e) => println!("Error: {}", e),
+    }
 }
 
 fn sampleMatch(ex: EnumEx) {
@@ -29,5 +35,12 @@ fn sampleOption(nmb: Option<i32>){
     match nmb{
         None => println!("None"),
         Some(x) => println!("{}", x),
+    }
+}
+
+fn h(i: i32) -> Result<i32, String> {
+    match i {
+        i if i >= 0 => Ok(i + 10),
+        _ => Err(format!("Input to h less that 0, found: {}", i))
     }
 }
